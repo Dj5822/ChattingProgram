5e6e5a8dc6e08fe1e2115adf2226aff63ab94c23
dj5822 <dj5822@gmail.com>
2021-10-14 15:01:23 +1300

    adjusted code to follow PEP8 more closely

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 
 
-class MyApp(QWidget):
+class ChatApp(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -10,7 +10,7 @@ class MyApp(QWidget):
         size = screen.size()
         self.width = size.width()
         self.height = size.height()
-        self.setupConnectionWindow("Chat Application")
+        self.setup_connection_window("Chat Application")
 
     """
     Used to move the window to the center of the screen.
@@ -24,45 +24,51 @@ class MyApp(QWidget):
     """
     Used to setup the GUI.
     """
-    def setupConnectionWindow(self, title):
+    def setup_connection_window(self, title):
         self.setWindowTitle(title)
         self.resize(self.width/2, self.height/2)   
         self.center_window()
 
         # Create components.
-        ipAddressLabel = QLabel('IP Address', self)
-        portLabel = QLabel('Port', self)
-        nicknameLabel = QLabel('Nick Name', self)
-        ipAddressTextBox = QLineEdit(self)
-        portTextBox = QLineEdit(self)
-        nicknameTextBox = QLineEdit(self)
-        connectButton = QPushButton('Connect')
-        cancelButton = QPushButton('Cancel')
+        self.ip_address_label = QLabel('IP Address', self)
+        self.port_label = QLabel('Port', self)
+        self.nickname_label = QLabel('Nick Name', self)
+        self.ip_address_textbox = QLineEdit(self)
+        self.port_textbox = QLineEdit(self)
+        self.nickname_textbox = QLineEdit(self)
+        self.connect_button = QPushButton('Connect')
+        self.cancel_button = QPushButton('Cancel')
+
+        # Add button functionality.
+        self.connect_button.clicked.connect(self.show_connected_window)
         
         # Add components to grid layout
-        gridLayout = QGridLayout()
-        gridLayout.addWidget(ipAddressLabel, 0, 0)
-        gridLayout.addWidget(portLabel, 1, 0)
-        gridLayout.addWidget(nicknameLabel, 2, 0)
-        gridLayout.addWidget(ipAddressTextBox, 0, 1)
-        gridLayout.addWidget(portTextBox, 1, 1)
-        gridLayout.addWidget(nicknameTextBox, 2, 1)
+        self.grid_layout = QGridLayout()
+        self.grid_layout.addWidget(self.ip_address_label, 0, 0)
+        self.grid_layout.addWidget(self.port_label, 1, 0)
+        self.grid_layout.addWidget(self.nickname_label, 2, 0)
+        self.grid_layout.addWidget(self.ip_address_textbox, 0, 1)
+        self.grid_layout.addWidget(self.port_textbox, 1, 1)
+        self.grid_layout.addWidget(self.nickname_textbox, 2, 1)
 
         # Add buttons to layout.
-        buttonLayout = QHBoxLayout()
-        buttonLayout.addWidget(connectButton)
-        buttonLayout.addWidget(cancelButton)
+        self.button_layout = QHBoxLayout()
+        self.button_layout.addWidget(self.connect_button)
+        self.button_layout.addWidget(self.cancel_button)
         
         # Add layouts to the parent layout.
-        parentLayout = QVBoxLayout()
-        parentLayout.addLayout(gridLayout)
-        parentLayout.addLayout(buttonLayout)
-        self.setLayout(parentLayout)
+        self.parent_layout = QVBoxLayout()
+        self.parent_layout.addLayout(self.grid_layout)
+        self.parent_layout.addLayout(self.button_layout)
+        self.setLayout(self.parent_layout)
 
         self.show()
+
+    def show_connected_window(self, checked):
+        None
 
 
 if __name__ == '__main__':
    app = QApplication(sys.argv)
-   ex = MyApp()
+   ex = ChatApp()
    sys.exit(app.exec_())
