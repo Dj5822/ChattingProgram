f2a6216633474efe29888bbc5f72c5ee8da079c2
dj5822 <dj5822@gmail.com>
2021-10-15 10:51:46 +1300

    Added one to one chat window

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt5.QtWidgets import *
 
-
+"""
+The first window that is shown at the start of the application.
+"""
 class ChatApp(QWidget):
 
     def __init__(self):
@@ -65,11 +67,15 @@ class ChatApp(QWidget):
 
         self.show()
 
+    # Used to show the connected window after connecting successfully.
     def show_connected_window(self):
         self.connected_window = ConnectedWindow(self.width, self.height, self.title)
         self.connected_window.show()
         self.hide()
 
+"""
+The window that is shown after successfully connecting.
+"""
 class ConnectedWindow(QWidget):
     def __init__(self, width, height, title):
         super().__init__()
@@ -78,12 +84,18 @@ class ConnectedWindow(QWidget):
         self.title = title
         self.setup_connected_window()
 
+    """
+    Used to move the window to the center of the screen.
+    """
     def center_window(self):
         qtRectangle = self.frameGeometry()
         centerPoint = QDesktopWidget().availableGeometry().center()
         qtRectangle.moveCenter(centerPoint)
         self.move(qtRectangle.topLeft())
 
+    """
+    Used to setup the GUI.
+    """
     def setup_connected_window(self):
         self.setWindowTitle(self.title)
         self.resize(self.width, self.height)   
@@ -98,6 +110,9 @@ class ConnectedWindow(QWidget):
         self.create_button = QPushButton('Create')
         self.join_button = QPushButton('Join')
         self.close_button = QPushButton('Close')
+
+        # Add button functionality.
+        self.one_to_one_chat_button.clicked.connect(self.show_chat_window)
 
         # Create layouts
         self.connected_clients_layout = QHBoxLayout()
@@ -119,6 +134,52 @@ class ConnectedWindow(QWidget):
         self.parent_layout.addWidget(self.close_button)
         self.setLayout(self.parent_layout)
 
+    def show_chat_window(self):
+        self.chat_room_window = ChatRoomWindow(self.width, self.height, self.title)
+        self.chat_room_window.show()
+        self.hide()
+
+"""
+The window that is shown after pressing 1:1 chat button.
+"""
+class ChatRoomWindow(QWidget):
+    def __init__(self, width, height, title):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.title = title
+        self.setup_chat_room_window()
+
+    def center_window(self):
+        qtRectangle = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())
+
+    def setup_chat_room_window(self):
+        self.setWindowTitle(self.title)
+        self.resize(self.width, self.height)   
+        self.center_window()
+
+        # Create components.
+        self.title_label = QLabel('Chat Title')
+        self.chat_text_browser = QTextBrowser()
+        self.chat_input = QLineEdit()
+        self.send_button = QPushButton('Send')
+        self.close_button = QPushButton('Close')
+
+        # Create layouts
+        self.chat_input_layout = QHBoxLayout()
+        self.parent_layout = QVBoxLayout()
+
+        # Add components to layouts
+        self.parent_layout.addWidget(self.title_label)
+        self.parent_layout.addWidget(self.chat_text_browser)
+        self.chat_input_layout.addWidget(self.chat_input)
+        self.chat_input_layout.addWidget(self.send_button)
+        self.parent_layout.addLayout(self.chat_input_layout)
+        self.parent_layout.addWidget(self.close_button)
+        self.setLayout(self.parent_layout)    
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
