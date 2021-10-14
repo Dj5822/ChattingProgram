bb353aae13a924f6bdc88a977749c1bc1bdc2d30
dj5822 <dj5822@gmail.com>
2021-10-14 14:36:06 +1300

    Added connection window

--- client.py
+++ client.py
@@ -1,91 +1,68 @@
-import select
-import socket
 import sys
-import signal
-import argparse
+from PyQt5.QtWidgets import *
 
-from utils import *
 
-SERVER_HOST = 'localhost'
+class MyApp(QWidget):
 
-class ChatClient():
-    """ A command line chat client using select """
-    def __init__(self, name, port, host=SERVER_HOST):
-        self.name = name
-        self.connected = False
-        self.host = host
-        self.port = port
+    def __init__(self):
+        super().__init__()
+        screen = app.primaryScreen()
+        size = screen.size()
+        self.width = size.width()
+        self.height = size.height()
+        self.setupConnectionWindow("Chat Application")
+
+    """
+    Used to move the window to the center of the screen.
+    """
+    def center_window(self):
+        qtRectangle = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())
+
+    """
+    Used to setup the GUI.
+    """
+    def setupConnectionWindow(self, title):
+        self.setWindowTitle(title)
+        self.resize(self.width/2, self.height/2)   
+        self.center_window()
+
+        # Create components.
+        ipAddressLabel = QLabel('IP Address', self)
+        portLabel = QLabel('Port', self)
+        nicknameLabel = QLabel('Nick Name', self)
+        ipAddressTextBox = QLineEdit(self)
+        portTextBox = QLineEdit(self)
+        nicknameTextBox = QLineEdit(self)
+        connectButton = QPushButton('Connect')
+        cancelButton = QPushButton('Cancel')
         
-        # Initial prompt
-        self.prompt = f'[{name}@{socket.gethostname()}]> '
+        # Add components to grid layout
+        gridLayout = QGridLayout()
+        gridLayout.addWidget(ipAddressLabel, 0, 0)
+        gridLayout.addWidget(portLabel, 1, 0)
+        gridLayout.addWidget(nicknameLabel, 2, 0)
+        gridLayout.addWidget(ipAddressTextBox, 0, 1)
+        gridLayout.addWidget(portTextBox, 1, 1)
+        gridLayout.addWidget(nicknameTextBox, 2, 1)
+
+        # Add buttons to layout.
+        buttonLayout = QHBoxLayout()
+        buttonLayout.addWidget(connectButton)
+        buttonLayout.addWidget(cancelButton)
         
-        # Connect to server at port
-        try:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.sock.connect((host, self.port))
-            print(f'Now connected to chat server@ port {self.port}')
-            self.connected = True
-            
-            # Send my name...
-            send(self.sock, 'NAME: ' + self.name)
-            data = receive(self.sock)
-            
-            # Contains client address, set it
-            addr = data.split('CLIENT: ')[1]
-            self.prompt = '[' + '@'.join((self.name, addr)) + ']> '
-        except socket.error as e:
-            print(f'Failed to connect to chat server @ port {self.port}')
-            sys.exit(1)
+        # Add layouts to the parent layout.
+        parentLayout = QVBoxLayout()
+        parentLayout.addLayout(gridLayout)
+        parentLayout.addLayout(buttonLayout)
+        self.setLayout(parentLayout)
 
-    def cleanup(self):
-        """Close the connection and wait for the thread to terminate."""
-        self.sock.close()
+        self.show()
 
-    def run(self):
-        """ Chat client main loop """
-        while self.connected:
-            try:
-                sys.stdout.write(self.prompt)
-                sys.stdout.flush()
 
-                # Wait for input from stdin and socket
-                readable, writeable, exceptional = select.select(
-                    [0, self.sock], [], [])
-
-                for sock in readable:
-                    if sock == 0:
-                        data = sys.stdin.readline().strip()
-                        if data == "GET_ALL_CLIENTS":
-                            send(self.sock, data)
-                            data = receive_clients(self.sock)
-                            sys.stdout.write(data + '\n')
-                            sys.stdout.flush()
-                        elif data:
-                            send(self.sock, data)
-                    elif sock == self.sock:
-                        data = receive(self.sock)
-                        if not data:
-                            print('Client shutting down.')
-                            self.connected = False
-                            break
-                        else:
-                            sys.stdout.write(data + '\n')
-                            sys.stdout.flush()
-
-            except KeyboardInterrupt:
-                print(" Client interrupted. """)
-                self.cleanup()
-                break
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--name', action="store", dest="name", required=True)
-    parser.add_argument('--port', action="store",
-                        dest="port", type=int, required=True)
-    given_args = parser.parse_args()
-
-    port = given_args.port
-    name = given_args.name
-
-    client = ChatClient(name=name, port=port)
-    client.run()
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   ex = MyApp()
+   sys.exit(app.exec_())
